01d347e3b7b4d40a4a6ebecd8fac6989a5f17a83
Rodriguez Padron, Emmanuel <emmanuelrodriguezpadron@my.unt.edu>
2024-07-16 14:53:21 -0500

    writing to clarify changes made

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+/* Import hooks from the react library */
 import React, { useEffect, useState } from 'react';
+/* Import styling from App.css */
 import './App.css';
+/* import axios for HTTP requests in order to get data from api */
 import axios from 'axios';
 
+/* */
+/* */
+/* */
+
+
 function App() {
+  /* 
+  timezones variable is initialized to empty array and function, setTimezones
+  updates the state, usestate initializes the state to empty array.
+  */
   const [timezones, setTimezones] = useState([]);
 
+  /* runs the hook function  */
   useEffect(() => {
+    /* represents an asynchronous function that fetches data from api */
     const fetchData = async () => {
       try {
         const timezonesResponse = await axios.get('https://v3.football.api-sports.io/timezone', {
@@ -25,6 +39,7 @@ function App() {
   }, []);
 
   return (
+    /* This div serves as the main container with a class name for styling */
     <div className="App">
       <header className="App-header">
         <p>Hello world</p>
